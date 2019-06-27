d24baf4bc416afad6101757504434b3ea1ac1fa0
silence <mrmanjunathams@gmail.com>
2019-06-27 10:56:10 +0530

    try

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 // import {Greet} from './components/Greet'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-     <Greet />
-     <Welcome />
+     <Greet name="Manjunatha M S"/>
+     <Greet name="Silence"/>
+     <Greet name="Hello!"/>
+
+     {/* <Welcome />
+     <Hello /> */}
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -5,6 +5,9 @@ import React from 'react'
 // }
 
 // export const Greet = () => <h1>Hello Manjunatha M S!</h1>
-const Greet = () => <h1>Hello Manjunatha M S!</h1>
+const Greet = (props) => {
+    console.log(props)
+    return <h1>Hello {props.name}!</h1>
+}
 
 export default Greet
