a2e2fb26d08f19ed42e8fdf471eda0a2cd6d4864
silence <mrmanjunathams@gmail.com>
2019-06-27 08:49:55 +0530

    class components

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Manjunatha M S!</h1>
+// }
+
+// export const Greet = () => <h1>Hello Manjunatha M S!</h1>
+const Greet = () => <h1>Hello Manjunatha M S!</h1>
+
+export default Greet
